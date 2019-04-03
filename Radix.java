5437ca5230dd91787d1e4f89fee8e52c51429640
bavrahami00 <bavrahami00@stuy.edu>
2019-04-02 21:42:57 -0400

    Trying out moving from list to list

--- Radix.java
+++ Radix.java
@@ -6,14 +6,22 @@ public class Radix {
       max = (int)Math.floor(Math.max(max,Math.log10(data[x])));
       set.add(data[x]);
     }
-    max += 2;
+    max++;
     @SuppressWarnings("unchecked")
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     for (int x = 0; x < 20; x++) {
       buckets[x] = new MyLinkedList<Integer>();
     }
+    int temp;
     for (int x = 0; x < max; x++) {
-
+      while (set.size() > 0) {
+        System.out.println(set);
+        temp = set.removeFront();
+        buckets[9+((temp/(int)Math.pow(10,x))%10)].add(temp);
+      }
+    }
+    for (int x = 0; x < buckets.length; x++) {
+      System.out.println(buckets[x]);
     }
   }
 }
